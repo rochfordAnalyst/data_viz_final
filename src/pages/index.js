9fef7b1ce56c4e0ece9c7c1531017ee44f09482e
rochfordAnalyst <brochford@fordham.edu>
2022-04-30 18:36:41 -0400

    newBUild

--- src/pages/index.js
+++ src/pages/index.js
@@ -84,7 +84,7 @@ const links = [
     text: "Tutorial",
     url: "https://www.gatsbyjs.com/docs/tutorial/",
     description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+      "lit shit. Designed to guide you through setting up your first Gatsby site.",
     color: "#E95800",
   },
   {
