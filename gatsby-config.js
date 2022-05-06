292190c5eea97d3486855e662474ff5426bd38ad
rochfordAnalyst <brochford@fordham.edu>
2022-05-06 00:10:33 -0400

    img

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,20 @@ module.exports = {
           path: `data_viz_final/src/data/`,
         },
       },
+      {
+        resolve: `gatsby-plugin-sharp`,
+        options: {
+          // Defaults used for gatsbyImageData and StaticImage
+          defaults: {},
+          // Set to false to allow builds to continue on image errors
+          failOnError: true,
+          // deprecated options and their defaults:
+          base64Width: 20,
+          forceBase64Format: ``, // valid formats: png,jpg,webp
+          useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+          stripMetadata: true,
+          defaultQuality: 50,
+        },
+      },
   ],
 }
