5164ef33f96e89fa41875579f244f813a796ae5b
rochfordAnalyst <brochford@fordham.edu>
2022-05-06 00:00:26 -0400

    new

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     title: `Fordham-Recursion-ESG-Big-Mortgage-Data-Research`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/src/data/`,
+        },
+      },
+  ],
 }
